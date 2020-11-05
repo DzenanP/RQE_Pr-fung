673fdc302fed9ae95d9677a6e15ca583f4725e6c
Dzenan <wi19b025@technikum-wien.at>
2020-11-05 09:53:09 +0100

    Version1

---
+++ RQE_Prüfung/SpecFlowFeature1.feature
@@ -0,0 +1,20 @@
+﻿Feature: SpecFlowFeature1
+	In order to avoid silly mistakes
+	As a math idiot
+	I want to be told the sum of two numbers
+
+@mytag
+Scenario: Sinus
+	Given the number is PI
+	When the sinus is taken
+	Then the result should be 0
+
+Scenario: Kosinus
+	Given the number is PI
+	When the kosinus is taken
+	Then the result should be -1
+
+Scenario: Tangens
+	Given the number is PI
+	When the tangens is taken
+	Then the result should be 0
---
+++ RQE_Prüfung/SpecFlowFeature1Steps.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Globalization;
+using TechTalk.SpecFlow;
+using Xunit;
+
+namespace Übung2
+{
+    [Binding]
+    public class SpecFlowFeature1Steps
+    {
+        double number, x;
+        [Given(@"the number is PI")]
+        public void GivenTheNumberIsPI()
+        {
+            number = Math.PI;
+        }
+        [When(@"the sinus is taken")]
+        public void WhenTheSinusIsTaken()
+        {
+            x = Math.Sin(number);
+        }
+        [When(@"the kosinus is taken")]
+        public void WhenTheKosinusIsTaken()
+        {
+            x = Math.Cos(number);
+        }
+        [When(@"the tangens is taken")]
+        public void WhenTheTangensIsTaken()
+        {
+            x = Math.Tan(number);
+        }
+        [Then(@"the result should be (.*)")]
+        public void ThenTheResultShouldBe(int p0)
+        {
+            Assert.Equal(p0, x, 5);
+        }
+    }
+}
